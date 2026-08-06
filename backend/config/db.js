bb06110bdd10e43cb1b64d38dce2dad3f68ffa04
Sadhik Salim <sadhik3656@gmail.com>
2026-08-06 17:08:05 +0530

    Debug MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO URI:", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Error");
-    console.error(error.message);
+    console.error(error);
     process.exit(1);
   }
 };
