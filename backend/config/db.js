b3a6e0b494088e157c245b258da8254fd8d836d0
Sadhik Salim <sadhik3656@gmail.com>
2026-08-06 17:20:19 +0530

    mongo uri error

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("MONGO URI:", process.env.MONGO_URI);
+    console.log("MONGO URI JSON:", JSON.stringify(process.env.MONGO_URI));
 
     await mongoose.connect(process.env.MONGO_URI);
 
